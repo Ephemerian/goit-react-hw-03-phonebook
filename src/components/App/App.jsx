3f08e68a50fb4cd2682108152d82dc298181cc5b
Ephemerian <106543055+Ephemerian@users.noreply.github.com>
2024-01-30 03:58:05 +0200

    created method for adding contacts in local storage

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -14,7 +14,20 @@ class App extends Component {
       {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
       {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},],
     filter: '',
-  } 
+  }
+  
+  componentDidMount() {
+    const stringifiedContacts = localStorage.getItem("contacts");
+    const parsedContacts = JSON.parse(stringifiedContacts) ?? [];
+    this.setState({ contacts: parsedContacts,});
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.contacts.length !== prevState.contacts.length) {
+      const stringifiedContacts = JSON.stringify(this.state.contacts);
+      localStorage.setItem("contacts", stringifiedContacts);
+    }
+  }
 
   handleAddContact = (contactData) => {
 
